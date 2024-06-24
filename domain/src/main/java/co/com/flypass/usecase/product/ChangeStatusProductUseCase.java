31d5584f84116ab28b6156a5a66a62c787535e5b
Andres Montoya <andresmontoyat@gmail.com>
2024-06-24 11:47:37 -0500

    fix: add validation for change status use case

--- domain/src/main/java/co/com/flypass/usecase/product/ChangeStatusProductUseCase.java
+++ domain/src/main/java/co/com/flypass/usecase/product/ChangeStatusProductUseCase.java
@@ -1,8 +1,10 @@
 package co.com.flypass.usecase.product;
 
 import co.com.flypass.domain.enums.ProductStatus;
+import co.com.flypass.exception.ConflictException;
 import co.com.flypass.exception.ResourceNotFoundException;
 import co.com.flypass.repository.ProductRepository;
+import java.math.BigDecimal;
 import java.math.BigInteger;
 
 public class ChangeStatusProductUseCase {
@@ -16,6 +18,10 @@ public class ChangeStatusProductUseCase {
   public void execute(BigInteger number, ProductStatus status) {
     var product = productRepository.findByNumber(number)
         .orElseThrow(() -> new ResourceNotFoundException("Product not found", number));
+
+    if (ProductStatus.CANCELED.equals(status) && !product.getBalance().equals(BigDecimal.ZERO)) {
+      throw new ConflictException("The Product cannot canceled because the product has balance, ", number, product.getBalance());
+    }
 
     product.setStatus(status);
     productRepository.update(product);
